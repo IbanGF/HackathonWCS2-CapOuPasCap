3d2da9a013f67640c7c17222dd65806523c81860
the-dsw <fmod@the-dsw.com>
2016-05-04 01:37:33 +0200

    test2

--- index.ios.js
+++ index.ios.js
@@ -37,7 +37,7 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#403197',
+    backgroundColor: '#F5FCFF',
   },
   welcome: {
     fontSize: 20,
